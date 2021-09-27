58129c785ef53135a7ba55fb415991bf002a1e3d
Liberbell <liberbell@gmail.com>
2021-09-27 09:18:35 +0900

    main2

--- pj1/main.go
+++ pj1/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
 func main() {
 	response, _ := http.Get("https://postman-echo.com/get")
+
+	responseBody, _ := ioutil.ReadAll(response.Body)
+
+	fmt.Println(response.Status)
+	fmt.Println(string(responseBody))
 }
