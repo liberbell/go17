6091e83ad761e86466e8b7e43e57d8503dd11936
Liberbell <liberbell@gmail.com>
2021-09-27 09:32:58 +0900

    main2

--- pj2/main.go
+++ pj2/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"io"
+	"net/http"
+)
+
 func main() {
-	testHTTPCall := func(responseWriter http.ResponseWriter, request *http.Request)
+	testHTTPCall := func(responseWriter http.ResponseWriter, request *http.Request) {
+		io.WriteString(responseWriter, "This is a test response.")
+	}
 }
