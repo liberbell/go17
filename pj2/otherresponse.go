71118afccd1dad6eb305cc721f9efc192ec6f31f
Liberbell <liberbell@gmail.com>
2021-09-28 08:35:07 +0900

    otherresponse2

--- pj2/otherresponse.go
+++ pj2/otherresponse.go
@@ -6,6 +6,6 @@ import (
 
 func main() {
 	testServer := httptest(http.HandleFunc(func(resonseWriter http.ResponseWriter, r http.Request) {
-
+		resonseWriter.WriteHeader(http.StatusNotFound)
 	}))
 }
