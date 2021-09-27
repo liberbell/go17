4a9b1e8778357c744d6bd6bc61831b447abf47f1
Liberbell <liberbell@gmail.com>
2021-09-28 08:36:19 +0900

    otherresponse3

--- pj2/otherresponse.go
+++ pj2/otherresponse.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"io"
 	"net/http"
+	"net/http/httptest"
 )
 
 func main() {
-	testServer := httptest(http.HandleFunc(func(resonseWriter http.ResponseWriter, r http.Request) {
+	testServer := httptest.NewServer(http.HandleFunc(func(resonseWriter http.ResponseWriter, r *http.Request) {
 		resonseWriter.WriteHeader(http.StatusNotFound)
+		io.WriteString(resonseWriter, "This is a test response.")
 	}))
 }
