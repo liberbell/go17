1765b7586b0f549dc3708b7a6303b2ea774b8898
Liberbell <liberbell@gmail.com>
2021-09-28 23:23:24 +0900

    html2

--- pj3/html/html.go
+++ pj3/html/html.go
@@ -1,5 +1,11 @@
 package html
 
+import "net/http"
+
 type CarHandler struct {
 	Car demo.Car
+}
+
+func (h *CarHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 }
