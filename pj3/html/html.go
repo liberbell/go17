f7f8d7826857fb3399bdf25c9a2991000d648e8a
Liberbell <liberbell@gmail.com>
2021-09-29 08:43:41 +0900

    html3

--- pj3/html/html.go
+++ pj3/html/html.go
@@ -13,7 +13,7 @@ type CarHandler struct {
 
 func (h *CarHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprintf(w, "<font color='green><b>Make:</b></font>   <i>%v</i><BR><font color='green'><b>Model:</b></font>   <i>%v</i><BR><font color='green'><b>")
+	fmt.Fprintf(w, "<font color='green><b>Make:</b></font>   <i>%v</i><BR><font color='green'><b>Model:</b></font>   <i>%v</i><BR><font color='green'><b>Year:</b></font> <i>%v</i>")
 }
 
 type PersonHandler struct {
