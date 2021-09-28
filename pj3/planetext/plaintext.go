f75e81427b0f2e72f810a7570534e5b272fa716d
Liberbell <liberbell@gmail.com>
2021-09-29 08:57:51 +0900

    plaintext7

--- pj3/planetext/plaintext.go
+++ pj3/planetext/plaintext.go
@@ -13,3 +13,7 @@ type CarHandler struct {
 func (h *CarHandler) ServeHTTP(w http.ResponseWriter, r http.Request) {
 	fmt.Fprintf(w "Make: %v -- Model: %v -- Year: %v", h.Car.Make, h.Car.Model, h.Car.Year)
 }
+
+type PersonHandler struct {
+	Person demo.Person
+}
