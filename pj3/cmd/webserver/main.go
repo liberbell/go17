31cf6c8517922e2387d6a3f4aa4fd9cfae0ed793
Liberbell <liberbell@gmail.com>
2021-09-29 09:11:01 +0900

    main4

--- pj3/cmd/webserver/main.go
+++ pj3/cmd/webserver/main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"net/http"
+
 	"demo.com/demo"
 	"demo.com/demo/html"
-	"demo.com/demo/plaintext"
 )
 
 func main() {
-	http.Handle("/person/html", &html.PersonHandler{demo.Person{"Bob, "Smith"}})
-	http.Handle("/car/html", )
+	http.Handle("/person/html", &html.PersonHandler{demo.Person{"Bob", "Smith"}})
+	http.Handle("/car/html", &html.CarHandler{demo.Car{"Ford", "Fiesta", 2005}})
 }
