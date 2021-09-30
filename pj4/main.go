c49e4cee6b19667e4282860e956d11c69800b9db
Liberbell <liberbell@gmail.com>
2021-10-01 08:49:16 +0900

    main2

--- pj4/main.go
+++ pj4/main.go
@@ -6,4 +6,8 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "html/unrecognized_path.html")
 	})
+
+	http.HandleFunc("/demopath1/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "html/demopath1.html")
+	})
 }
