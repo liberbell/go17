ccc26bf21f54d3fc153b258f5ebb6605efa40f00
Liberbell <liberbell@gmail.com>
2021-10-01 09:09:24 +0900

    main6

--- pj5/main.go
+++ pj5/main.go
@@ -3,12 +3,12 @@ package main
 import "net/http"
 
 func main() {
-	fs := http.FileServer(http.Dir("/pdf"))
+	fs := http.FileServer(http.Dir("/pj5"))
 
 	http.Handle("/projectfiles/", http.StripPrefix("/projectfiles", fs))
 
 	http.HandleFunc("samplepdf", func(res http.ResponseWriter, req *http.Request) {
-		http.ServeFile(res, req, "/pdf/matplotlib.pdf")
+		http.ServeFile(res, req, "/pj5/pdf/matplotlib.pdf")
 	})
 
 	http.ListenAndServe(":8001", nil)
