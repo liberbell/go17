cac2c16006dc31ca411616aa2c01ba1cefe7bbdf
Liberbell <liberbell@gmail.com>
2021-10-02 14:25:26 +0900

    main2

--- pj9/main.go
+++ pj9/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func NameHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Name: %s %s", p.ByName("firstname"), p.ByName("lastname"))
+}
+
 func main() {
 	router := httprouter.New()
 }
