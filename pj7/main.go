ccea7a6160508d9d60dc7faf6edf9cfcafb40cb3
Liberbell <liberbell@gmail.com>
2021-10-02 10:45:35 +0900

    main2

--- pj7/main.go
+++ pj7/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 type PersonHandler struct {
 	FirstName string
@@ -11,6 +14,14 @@ type CarHandler struct {
 	Make  string
 	Model string
 	Year  int
+}
+
+func (h *PersonHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Name: %v %v", h.FirstName, h.LastName)
+}
+
+func (c *CarHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Make: %v  -- Model: %v -- Year: %v", c.Make, c.Model, c.Year)
 }
 
 func main() {
