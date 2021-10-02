ab09775b9102db4b9354bdab33cb83841ff6b6d1
Liberbell <liberbell@gmail.com>
2021-10-02 10:58:47 +0900

    main3

--- pj8/main.go
+++ pj8/main.go
@@ -9,6 +9,10 @@ func CustomHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "This is a custom handler.")
 }
 
+func LogRequests(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request))
+}
+
 func main() {
 	serveMux := http.ServeMux()
 	serveMux.HandleFunc("/custom/", CustomHandler)
